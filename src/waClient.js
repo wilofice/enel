a41c431c60ce9119e6355397af43852140953ce4
ALAHASSA Généreux <wilofice@gmail.com>
2026-03-08 07:36:52 +0100

    fix: resolve WhatsApp ProtocolError using stable remote webVersionCache

--- src/waClient.js
+++ src/waClient.js
@@ -80,7 +80,15 @@ async function handleIncoming(client, msg) {
 function initWhatsApp() {
   return new Promise((resolve, reject) => {
     const client = new Client({
-      authStrategy: new LocalAuth()
+      authStrategy: new LocalAuth(),
+      puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true,
+      },
+      webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+      }
     });
 
     client.on('qr', (qr) => {
